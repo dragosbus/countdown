65855b33b3087a38ff4c57f827f41dcdc94b9308
dragosbus <dragosbusuioc94@gmail.com>
2018-07-06 14:18:55 +0200

    added comments

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,8 +6,10 @@ export const Timer = props =>{
     let days = props.date / (1000 * 3600 * 24);
     let hours = (days - parseInt(days)) * 24;
     let minutes = (hours - parseInt(hours)) * 60;
-    let seconds = props.date !== 0 ? (minutes - parseInt(minutes)) * 60 : 0;
-
+    let seconds = (minutes - parseInt(minutes)) * 60;
+    //because the decrementer is called when the component is called
+    //the seconds become negative
+    //so, when is negative the value become 0
     return(
         <div className="timer">
             <p>{parseInt(days)} days</p>
